cf2245cf22409bc39bb54533209992b69cf7e383
Isilendil <isilendil0815@gmail.com>
2015-05-28 09:58:37 +0800

    201505280959

--- HetTransfer2.m
+++ HetTransfer2.m
@@ -18,7 +18,7 @@ function  HetTransfer2(data_file, similarity_file)
 %--------------------------------------------------------------------------
 
 %load dataset
-load(sprintf('data/%s', data_file));
+load(sprintf('%s', data_file));
 %load(sprintf('%s','boats_toy'));
 %load(sprintf('%s','flowers_tree'));
 %load(sprintf('%s','vehicle_tree'));
@@ -40,7 +40,7 @@ end
 
 Y = image_gnd(1:num_new,:);
 Y = full(Y);
-X = image_fea(1:num_new,:);
+X = image_fea_2(1:num_new,:);
 
 [n,d] = size(X);
 %[n,d] = size(image_fea);
@@ -91,50 +91,50 @@ for i=1:size(ID_new,1),
     time_PA2(i) = run_time;
     mistakes_list_PA2(i,:) = mistakes;
     
-    %2. HetTransfer-I-fixed
-    [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL12fixed(Y,Kernel,P_it,text_gnd,options,ID);
-    nSV_HT12fixed(i) = length(classifier.SV1)+length(classifier.SV2);
-    err_HT12fixed(i) = err_count;
-    time_HT12fixed(i) = run_time;
-    mistakes_list_HT12fixed(i,:) = mistakes;
-    
-    %3. HetTransfer-II-fixed
-    [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL22fixed(Y,Kernel,P_it,text_gnd,options,ID);
-    nSV_HT22fixed(i) = length(classifier.SV1)+length(classifier.SV2);
-    err_HT22fixed(i) = err_count;
-    time_HT22fixed(i) = run_time;
-    mistakes_list_HT22fixed(i,:) = mistakes;
-
-    %4. HetTransfer-I
-    [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL12(Y,Kernel,P_it,text_gnd,options,ID);
-    nSV_HT12(i) = length(classifier.SV1)+length(classifier.SV2);
-    err_HT12(i) = err_count;
-    time_HT12(i) = run_time;
-    mistakes_list_HT12(i,:) = mistakes;
-    
-    %5. HetTransfer-II
-    [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL22(Y,Kernel,P_it,text_gnd,options,ID);
-    nSV_HT22(i) = length(classifier.SV1)+length(classifier.SV2);
-    err_HT22(i) = err_count;
-    time_HT22(i) = run_time;
-    mistakes_list_HT22(i,:) = mistakes;
-    
+%     %2. HetTransfer-I-fixed
+%     [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL12fixed(Y,Kernel,P_it,text_gnd,options,ID);
+%     nSV_HT12fixed(i) = length(classifier.SV1)+length(classifier.SV2);
+%     err_HT12fixed(i) = err_count;
+%     time_HT12fixed(i) = run_time;
+%     mistakes_list_HT12fixed(i,:) = mistakes;
+%     
+%     %3. HetTransfer-II-fixed
+%     [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL22fixed(Y,Kernel,P_it,text_gnd,options,ID);
+%     nSV_HT22fixed(i) = length(classifier.SV1)+length(classifier.SV2);
+%     err_HT22fixed(i) = err_count;
+%     time_HT22fixed(i) = run_time;
+%     mistakes_list_HT22fixed(i,:) = mistakes;
+% 
+%     %4. HetTransfer-I
+%     [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL12(Y,Kernel,P_it,text_gnd,options,ID);
+%     nSV_HT12(i) = length(classifier.SV1)+length(classifier.SV2);
+%     err_HT12(i) = err_count;
+%     time_HT12(i) = run_time;
+%     mistakes_list_HT12(i,:) = mistakes;
+%     
+%     %5. HetTransfer-II
+%     [classifier, err_count, run_time, mistakes, mistakes_idx, SVs, TMs] = HetOTL22(Y,Kernel,P_it,text_gnd,options,ID);
+%     nSV_HT22(i) = length(classifier.SV1)+length(classifier.SV2);
+%     err_HT22(i) = err_count;
+%     time_HT22(i) = run_time;
+%     mistakes_list_HT22(i,:) = mistakes;
+%     
 
 end
 
 
 stat_file = sprintf('stat/%s-stat', data_file);
-save(stat_file, 'err_PA2', 'time_PA2', 'mistakes_list_PA2', 'err_HT12fixed', 'time_HT12fixed', 'mistakes_list_HT12fixed', 'err_HT22fixed', 'time_HT22fixed', 'mistakes_list_HT22fixed', 'err_HT12', 'time_HT12', 'mistakes_list_HT12', 'err_HT22', 'time_HT22', 'mistakes_list_HT22');
-
+%save(stat_file, 'err_PA2', 'time_PA2', 'mistakes_list_PA2', 'err_HT12fixed', 'time_HT12fixed', 'mistakes_list_HT12fixed', 'err_HT22fixed', 'time_HT22fixed', 'mistakes_list_HT22fixed', 'err_HT12', 'time_HT12', 'mistakes_list_HT12', 'err_HT22', 'time_HT22', 'mistakes_list_HT22');
+save('stat-birds-food-2', 'err_PA2', 'mistakes_list_PA2');
 
 
 fprintf(1,'-------------------------------------------------------------------------------\n');
 fprintf(1,'number of mistakes, cpu running time\n');
 fprintf(1,'PA             %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_PA2)/m*100,  std(err_PA2)/m*100, mean(time_PA2), std(time_PA2));
-fprintf(1,'OHT-I-fixed  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT12fixed)/m*100,   std(err_HT12fixed)/m*100, mean(time_HT12fixed), std(time_HT12fixed));
-fprintf(1,'OHT-II-fixed  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT22fixed)/m*100,   std(err_HT22fixed)/m*100, mean(time_HT22fixed), std(time_HT22fixed));
-fprintf(1,'OHT-I  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT12)/m*100,   std(err_HT12)/m*100, mean(time_HT12), std(time_HT12));
-fprintf(1,'OHT-II  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT22)/m*100,   std(err_HT22)/m*100, mean(time_HT22), std(time_HT22));
+%fprintf(1,'OHT-I-fixed  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT12fixed)/m*100,   std(err_HT12fixed)/m*100, mean(time_HT12fixed), std(time_HT12fixed));
+%fprintf(1,'OHT-II-fixed  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT22fixed)/m*100,   std(err_HT22fixed)/m*100, mean(time_HT22fixed), std(time_HT22fixed));
+%fprintf(1,'OHT-I  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT12)/m*100,   std(err_HT12)/m*100, mean(time_HT12), std(time_HT12));
+%fprintf(1,'OHT-II  %.4f \t %.4f \t %.4f \t %.4f\n', mean(err_HT22)/m*100,   std(err_HT22)/m*100, mean(time_HT22), std(time_HT22));
 fprintf(1,'-------------------------------------------------------------------------------\n');
 
 
---
+++ run_feature2.m
@@ -0,0 +1,138 @@
+
+data_file = 'birds-food';
+feature2(data_file );
+
+data_file = 'birds-sun';
+feature2(data_file );
+
+data_file = 'birds-tower';
+feature2(data_file );
+
+data_file = 'birds-toy';
+feature2(data_file );
+
+data_file = 'boats-birds';
+feature2(data_file );
+
+data_file = 'boats-food';
+feature2(data_file );
+
+data_file = 'boats-sun';
+feature2(data_file );
+
+data_file = 'boats-tower';
+feature2(data_file );
+
+data_file = 'boats-toy';
+feature2(data_file );
+
+data_file = 'flowers-birds';
+feature2(data_file );
+
+data_file = 'flowers-boats';
+feature2(data_file );
+
+data_file = 'flowers-food';
+feature2(data_file );
+
+data_file = 'flowers-rocks';
+feature2(data_file );
+
+data_file = 'flowers-sun';
+feature2(data_file );
+
+data_file = 'flowers-tower';
+feature2(data_file );
+
+data_file = 'flowers-toy';
+feature2(data_file );
+
+data_file = 'flowers-tree';
+feature2(data_file );
+
+data_file = 'flowers-vehicle';
+feature2(data_file );
+
+data_file = 'food-tower';
+feature2(data_file );
+
+data_file = 'rocks-birds';
+feature2(data_file );
+
+data_file = 'rocks-boats';
+feature2(data_file );
+
+data_file = 'rocks-food';
+feature2(data_file );
+
+data_file = 'rocks-sun';
+feature2(data_file );
+
+data_file = 'rocks-tower';
+feature2(data_file );
+
+data_file = 'rocks-toy';
+feature2(data_file );
+
+data_file = 'rocks-tree';
+feature2(data_file );
+
+data_file = 'rocks-vehicle';
+feature2(data_file );
+
+data_file = 'sun-food';
+feature2(data_file );
+
+data_file = 'sun-tower';
+feature2(data_file );
+
+data_file = 'sun-toy';
+feature2(data_file );
+
+data_file = 'toy-food';
+feature2(data_file );
+
+data_file = 'toy-tower';
+feature2(data_file );
+
+data_file = 'tree-birds';
+feature2(data_file );
+
+data_file = 'tree-boats';
+feature2(data_file );
+
+data_file = 'tree-food';
+feature2(data_file );
+
+data_file = 'tree-sun';
+feature2(data_file );
+
+data_file = 'tree-tower';
+feature2(data_file );
+
+data_file = 'tree-toy';
+feature2(data_file );
+
+data_file = 'vehicle-birds';
+feature2(data_file );
+
+data_file = 'vehicle-boats';
+feature2(data_file );
+
+data_file = 'vehicle-food';
+feature2(data_file );
+
+data_file = 'vehicle-sun';
+feature2(data_file );
+
+data_file = 'vehicle-tower';
+feature2(data_file )
+
+data_file = 'vehicle-toy';
+feature2(data_file );
+
+data_file = 'vehicle-tree';
+feature2(data_file );
+
+
+
